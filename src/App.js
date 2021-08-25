1e249f7ae75de97fac673758ab43f638431bd962
Jayme Said <jaymesaidbarbosahernandez@gmail.com>
2021-08-24 23:23:12 -0500

    inicio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const api = {
+  base: `https://digimon-api.vercel.app/api/digimon/name/`,
+}
+
+const App = () => {
+
+  const searchDigimon = (e) => {
+    if(e.key === "Enter") {
+      fetch(`${api.base}${digimon}`)
+      .then(res => res.json())
+      .then(result => {
+        setInfo(result)
+        console.log(result[0])
+        // console.log(result[0].name)
+      })
+    }
+  }
+
+  const search = (e) => {
+    searchDigimon(e)
+  }
+  
+  const [digimon, setDigimon] = useState("")
+  const [info, setInfo] = useState()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"
+        className=""
+        placeholder="Search city"
+        onChange={e => setDigimon(e.target.value)}
+        value={digimon}
+        onKeyPress={search} 
+        />
+
+
+        {info && 
+        
+        <div>
+          <p> {info[0].name} </p>
+          <img  src={`${info[0].img}`} alt="digimon" />
+          <p> {info[0].level} </p>
+        </div>}
     </div>
   );
 }
